96cbc7f18631ee5e1ac0a5695e559aaf27168ef8
Taras Mankovski <taras@frontside.com>
2024-09-14 13:12:18 -0400

    Remove unused import

--- task-buffer/task-buffer.test.ts
+++ task-buffer/task-buffer.test.ts
@@ -1,4 +1,4 @@
-import { run, sleep, spawn, suspend, type Task } from "effection";
+import { run, sleep, spawn, type Task } from "effection";
 import { describe, it } from "bdd";
 import { expect } from "expect";
 import { useTaskBuffer } from "./task-buffer.ts";
