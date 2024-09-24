f35bbf87e7699c38e8d7374a371999c1cfabd861
Taras Mankovski <taras@frontside.com>
2024-09-23 22:25:24 -0400

    Added index and detail page route

---
+++ www/hooks/use-package.ts
@@ -0,0 +1,92 @@
+import { evaluate } from "@mdx-js/mdx";
+import { join, resolve } from "@std/path";
+import { call, type Operation } from "effection";
+import rehypeInferDescriptionMeta from "rehype-infer-description-meta";
+import rehypePrismPlus from "rehype-prism-plus";
+import rehypeStringify from "rehype-stringify";
+import remarkGfm from "remark-gfm";
+import remarkParse from 'remark-parse';
+import remarkRehype from "remark-rehype";
+import { Fragment, jsx, jsxs } from "revolution/jsx-runtime";
+import { unified } from "unified";
+import type { VFile } from "vfile";
+import { z } from "zod";
+
+export interface Package {
+  path: string;
+  dirname: string;
+  packageName: string;
+  readme: string;
+  exports: string | Record<string, string>;
+  MDXContent: () => JSX.Element;
+  MDXDescription: () => JSX.Element;
+}
+
+const DenoJson = z.object({
+  name: z.string(),
+  version: z.optional(z.string()),
+  exports: z.union([z.record(z.string()), z.string()]),
+  private: z.union([z.undefined(), z.literal(true)]),
+});
+
+
+export class PrivatePackageError extends Error {
+  constructor(packageName: string) {
+    super(`Could not import ${packageName} because it's private`)
+  }
+}
+
+export function* usePackage(workspace: string): Operation<Package> {
+  const workspacePath = resolve(
+    import.meta.dirname ?? "",
+    `../../${workspace}`,
+  );
+
+  const config: { default: unknown } = yield* call(
+    () => import(`../../${workspace}/deno.json`, { with: { type: "json" } }),
+  );
+
+  const denoJson = DenoJson.parse(config.default);
+
+  if (denoJson.private === true) {
+    throw new PrivatePackageError(workspace);
+  } else {
+    const readme = yield* call(() =>
+      Deno.readTextFile(join(workspacePath, "README.md"))
+    );
+
+    let mod = yield* call(() =>
+      evaluate(readme, {
+        jsx,
+        jsxs,
+        jsxDEV: jsx,
+        Fragment,
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [[rehypePrismPlus, { showLineNumbers: true }]],
+      })
+    );
+
+    const content = mod.default({});
+
+    let file: VFile = yield* call(() =>
+      unified()
+        .use(remarkParse)
+        .use(remarkRehype)
+        .use(rehypeStringify)
+        .use(rehypeInferDescriptionMeta, { inferDescriptionHast: true, truncateSize: 400 })
+        .process(
+          readme
+        )
+    );
+
+    return {
+      dirname: workspace.replace("./", ""),
+      path: workspacePath,
+      packageName: denoJson.name,
+      exports: denoJson.exports,
+      readme,
+      MDXContent: () => content,
+      MDXDescription: () => file.data.meta.description
+    }
+  }
+}
--- www/routes/module.tsx
+++ www/routes/module.tsx
@@ -1,7 +1,43 @@
-import type { JSXHandler } from "revolution";
+import { type JSXHandler, useParams } from "revolution";
+import { usePackage } from "../hooks/use-package.ts";
+import { useAppHtml } from "./app.html.tsx";
+import { Package } from "../hooks/use-package.ts";
 
 export function moduleRoute(): JSXHandler {
   return function* () {
-    return <></>;
+    const params = yield* useParams<{ moduleName: string }>();
+
+    let pkg: Package;
+    try {
+      pkg = yield* usePackage(params.moduleName);
+    } catch (e) {
+      const AppHTML = yield* useAppHtml({
+        title: `${params.moduleName}`,
+        description: `Not found`,
+        pageTitle: `${params.moduleName} not found`,
+      });
+      return (
+        <AppHTML>
+          <p>
+            {params.moduleName} not found
+          </p>
+        </AppHTML>
+      );
+    }
+
+    const AppHTML = yield* useAppHtml({
+      title: `${pkg.packageName}`,
+      description: `${pkg.MDXDescription()}`,
+      pageTitle: `${pkg.packageName} | Effection Contribs`,
+    });
+
+    return (
+      <AppHTML>
+        <>
+          <h1>{pkg.packageName}</h1>
+          <p>{<pkg.MDXDescription />}</p>
+        </>
+      </AppHTML>
+    );
   };
 }
