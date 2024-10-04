79f339477c22fd5ffd3615dfb5ef58bb7fc2a56d
Taras Mankovski <taras@frontside.com>
2024-10-04 15:54:44 -0400

    Update fmt

--- www/tasks/publish-matrix.ts
+++ www/tasks/publish-matrix.ts
@@ -1,11 +1,12 @@
 import { main } from "effection";
 import { usePackages } from "../hooks/use-packages.ts";
 
-await main(function*() {
-
+await main(function* () {
   const packages = yield* usePackages();
-  
-  const includeStatement = { include: packages.map(pkg => ({ workspace: pkg.workspace })) };
+
+  const includeStatement = {
+    include: packages.map((pkg) => ({ workspace: pkg.workspace })),
+  };
 
   console.log(`::set-output name=matrix::${JSON.stringify(includeStatement)}`);
 });
