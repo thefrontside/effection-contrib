c969031aad01dcb4b08052a306703e348ce4ab79
Taras Mankovski <taras@frontside.com>
2024-10-04 12:05:35 -0400

    Only show exports once

--- www/components/exports.tsx
+++ www/components/exports.tsx
@@ -16,7 +16,7 @@ export function Exports({ pkg }: { pkg: Package }): JSXElement {
           <PackageExport
             packageName={pkg.packageName}
             exportName={exportName}
-            doc={pkg.docs[exportName]}
+            docs={pkg.docs[exportName]}
           />
         ))}
       </>
@@ -27,35 +27,46 @@ export function Exports({ pkg }: { pkg: Package }): JSXElement {
 interface PackageExportOptions {
   packageName: string;
   exportName: string;
-  doc: Array<RenderableDocNode>;
+  docs: Array<RenderableDocNode>;
 }
 
-function PackageExport({ packageName, exportName, doc }: PackageExportOptions) {
-  const exports = doc.flatMap((doc) => {
-    if (doc.declarationKind === "export") {
-      return [
-        <a
-          class="no-underline text-slate-300 hover:underline underline-offset-4"
-          href={`#${doc.id}`}
-        >
-          {["enum", "typeAlias", "namespace", "interface"].includes(doc.kind)
-            ? <Keyword>{"type "}</Keyword>
-            : ""}
-          {doc.name}
-        </a>,
-        ", ",
-      ];
-    } else {
-      return [];
-    }
-  }).slice(0, -1);
+function PackageExport(
+  { packageName, exportName, docs }: PackageExportOptions,
+) {
+  const exports = docs
+    .filter((doc) => doc.declarationKind === "export");
+
+  const names = exports.map((doc) => doc.name);
+
+  const unique = exports
+    .flatMap(
+      (doc, index) => {
+        if (names.indexOf(doc.name) === index) {
+          return [doc];
+        } else {
+          return [];
+        }
+      },
+    )
+    .flatMap((doc) => [
+      <a
+        class="no-underline text-slate-300 hover:underline underline-offset-4"
+        href={`#${doc.id}`}
+      >
+        {["enum", "typeAlias", "namespace", "interface"].includes(doc.kind)
+          ? <Keyword>{"type "}</Keyword>
+          : ""}
+        {doc.name}
+      </a>,
+      ", ",
+    ]).slice(0, -1);
 
   return (
     <pre class="language-ts">
       <code class="language-ts code-highlight">
         <Keyword>import</Keyword>
         <Punctuation>{" { "}</Punctuation>
-        <ul class="my-0 list-none pl-4">{chunk(exports).map(chunk => <li class="my-1 pl-0">{chunk}</li>)}</ul>
+        <ul class="my-0 list-none pl-4">{chunk(unique).map(chunk => <li class="my-1 pl-0">{chunk}</li>)}</ul>
         <Punctuation>{"} "}</Punctuation>
         <Keyword>{"from "}</Keyword>
         <span class="token string">"{join(packageName, exportName)}"</span>
