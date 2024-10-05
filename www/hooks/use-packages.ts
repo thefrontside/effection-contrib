0fbfac40d4fd1a746392f49a2fc185a2f8d9b596
Taras Mankovski <taras@frontside.com>
2024-10-05 19:35:33 -0400

    try to log the error

--- www/hooks/use-packages.ts
+++ www/hooks/use-packages.ts
@@ -3,12 +3,17 @@ import { type Package, usePackage } from "./use-package.tsx";
 import { PrivatePackageError } from "../errors.ts";
 
 export function* usePackages(): Operation<Package[]> {
-  const root = yield* call(() =>
-    import("../../deno.json", { with: { type: "json" } })
-  );
+  const root = yield* call(async () => {
+    try {
+      const denoJson = await import("../../deno.json", { with: { type: "json" } })
+      return denoJson;
+    } catch (e) {
+      console.error(e);
+    }
+  });
 
   const workspaces: Package[] = [];
-  for (let workspace of root.default.workspace) {
+  for (let workspace of root?.default?.workspace ?? []) {
     try {
       const pkg = yield* usePackage(workspace);
       workspaces.push(pkg);
