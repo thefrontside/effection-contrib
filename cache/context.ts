0c34a724688a67faa09dd349600ef305c683e79a
Taras Mankovski <taras@frontside.com>
2024-11-24 17:08:31 -0500

    Formatted context

--- cache/context.ts
+++ cache/context.ts
@@ -1,4 +1,8 @@
-import { createContext, type Operation, type Context } from "npm:effection@4.0.0-alpha.3";
+import {
+  type Context,
+  createContext,
+  type Operation,
+} from "npm:effection@4.0.0-alpha.3";
 import { join } from "jsr:@std/path@1.0.8";
 import type { Cache } from "./types.ts";
 import { JSONLCache } from "./mod.ts";
@@ -7,7 +11,10 @@ const DEFAULT_CACHE: Cache = JSONLCache.from({
   location: join(import.meta.dirname ?? Deno.cwd(), ".cache"),
 });
 
-export const CacheContext: Context<Cache> = createContext<Cache>("cache", DEFAULT_CACHE);
+export const CacheContext: Context<Cache> = createContext<Cache>(
+  "cache",
+  DEFAULT_CACHE,
+);
 
 export function* useCache(): Operation<Cache> {
   return yield* CacheContext.expect();
