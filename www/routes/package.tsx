ea7b27bc704900d9b5ea3b7f015c1c0741a3b4d4
Taras Mankovski <taras@frontside.com>
2024-12-15 20:24:59 -0500

    Added support environments

--- www/routes/package.tsx
+++ www/routes/package.tsx
@@ -51,7 +51,7 @@ export function packageRoute(): SitemapRoute<JSXElement> {
                     {yield* API()()}
                   </div>
                 </article>
-                <aside class="max-lg:row-start-1 lg:col-[span_3/_-1] lg:top-0 lg:sticky lg:max-h-screen flex flex-col box-border gap-y-4 -mt-4 pt-4">
+                <aside class="lg:col-[span_3/_-1] lg:top-0 lg:sticky lg:max-h-screen flex flex-col box-border gap-y-4 -mt-4 pt-4">
                   {yield* ScoreCard()()}
                 </aside>
               </div>
