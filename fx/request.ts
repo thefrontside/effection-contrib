9ff65869c7f5efc06dc353f2715bd6604fa98b69
Taras Mankovski <taras@frontside.com>
2025-01-09 20:58:35 -0500

    Fixed fmt

--- fx/request.ts
+++ fx/request.ts
@@ -1,6 +1,9 @@
 import { call, type Operation, useAbortSignal } from "npm:effection@3.0.3";
 
-export function* request(url: string | URL | Request, opts?: RequestInit): Operation<Response> {
+export function* request(
+  url: string | URL | Request,
+  opts?: RequestInit,
+): Operation<Response> {
   const signal = yield* useAbortSignal();
   const response = yield* call(fetch(url, { signal, ...opts }));
   return response;
