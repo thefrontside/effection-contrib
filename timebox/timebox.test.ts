4f5c9d8d19378e8f68d6813080fc4b11843abeaf
Charles Lowell <cowboyd@frontside.com>
2025-01-19 07:33:54 +0000

    🎉 Add @effection-contrib/timebox
    Adding time constraints to any operation without having to change it at
    all is one of the most powerful demonstrations of how Effection
    composes. It is also needed to implement convergent assertions which
    we will need in many different locations, and which we may want to
    release as a library in of itself.
    
    This takes the essence of all the timeout implementations over the
    last 5 years and puts them into generally available "timebox" package.
    A key decision that makes this implementation more flexible than
    earlier implementations is it does not throw a "TimeoutError" when the
    operation exceeds the deadline. Rather, it returns a discriminated
    union indicating whether it completed successfuly, or if it timed out.

---
+++ timebox/timebox.test.ts
@@ -0,0 +1,47 @@
+import { describe, it } from "bdd";
+import { expect } from "expect";
+import { timebox } from "./mod.ts";
+import {
+  type Operation,
+  run,
+  sleep,
+  suspend,
+} from "npm:effection@^4.0.0-alpha.4";
+
+describe("timebox", () => {
+  it("is completed if operation returns within alloted time", async () => {
+    let outcome = await run(() => timebox(100, delayed(5, () => "hello")));
+    outcome.timeout;
+    expect(outcome).toMatchObject({
+      timeout: false,
+      value: "hello",
+    });
+  });
+
+  it("is completed if operation throws within alloted time", async () => {
+    let task = run(() =>
+      timebox(
+        100,
+        delayed(5, () => {
+          throw new Error("boom!");
+        }),
+      )
+    );
+    await expect(task).rejects.toMatchObject({
+      message: "boom!",
+    });
+  });
+
+  it("is timed out if operation does not return within alloted time", async () => {
+    await expect(run(() => timebox(10, suspend))).resolves.toMatchObject({
+      timeout: true,
+    });
+  });
+});
+
+function delayed<T>(delayMS: number, value: () => T): () => Operation<T> {
+  return function* () {
+    yield* sleep(delayMS);
+    return value();
+  };
+}
